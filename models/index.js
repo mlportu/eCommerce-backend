4f60b642cbed289e40df26ff9ba2f5a2530c14be
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-10 10:07:27 -0500

    models index and associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,45 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'cat_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'prod_tag_id',
+    foreignKey: 'product_id'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'prod_tag_id',
+    foreignKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
